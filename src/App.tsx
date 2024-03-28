c2ae2a68979554dce02356a18d655e01f61748c9
Mabakal <badjokoffi1@gmail.com>
2024-03-28 20:35:11 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <p>Thanks you for visiting my website</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
